6b10e1b6f880235883e09597270eea5d87851976
Charanjeeet <40071826+Charanjeeet@users.noreply.github.com>
2018-06-26 20:20:35 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##This function Function makeCacheMatrix gets a matrix as an input, set the value of the matrix,
+ -#get the value of the matrix, set the inverse Matrix and get the inverse Matrix. The matrix object
+ -#can cache its own object. 
+ -
+ -#<<- operator is used to assign a value to an object in an environment that is different 
+ -#from the current environment 
+ -
+ -#take the matrix as an input
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
@@ -16,7 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+-## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an 
+ -# input and checks inverse matrix from makeCacheMatrix(matrix) has any value in it or not.
+ -# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data from 
+ -# and set the invertible  matrix by using the solve function.
+ -# In case inverse matrix from makeCacheMatrix((matrix) has some value in it (always works
+ -#after running the code 1st time), it returns a message  "Getting Cached Invertible Matrix" 
+ -#and the cached object
+ -  
+ -
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
